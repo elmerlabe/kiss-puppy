460df93a6c4edebf1a02c5304090fe463f515ad2
Elmer <elmerlabe@gmail.com>
2019-10-24 07:37:29 +0800

    add admin directory to eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(e) {
     e.addPassthroughCopy('static');
+    e.addPassthroughCopy('admin');
 
     e.addCollection("posts", function(collection) {
         return collection.getFilteredByTag("post").reverse();
