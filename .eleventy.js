d2405867682325c2c0a74669cb27ec1135620240
Elmer <elmerlabe@gmail.com>
2019-10-24 16:45:11 +0800

    edit file for news in home page

--- .eleventy.js
+++ .eleventy.js
@@ -7,9 +7,6 @@ module.exports = function(e) {
     });
 
     e.addFilter("head", function(array, n) {
-        if (n < 0) {
-            return array.slice(n);
-        }
         return array.slice(0, n);
     });
 
