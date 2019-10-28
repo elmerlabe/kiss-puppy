ab5e92bac2c1441997026c088df48b06c58325ff
Elmer <elmerlabe@gmail.com>
2019-10-28 11:29:37 +0800

    add pages directory for index cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 module.exports = function(e) {
     e.addPassthroughCopy('static');
     e.addPassthroughCopy('admin');
+    e.addPassthroughCopy('pages');
 
     e.addCollection("posts", function(collection) {
         return collection.getFilteredByTag("post").reverse();
     });
+
 
     e.addFilter("head", function(array, n) {
         return array.slice(0, n);
---
+++ pages/bio/bio.md
@@ -0,0 +1,7 @@
+---
+tags: bio
+title: Biography
+image: /static/img/img_biography.png
+
+content: The quick brown fox jumps over the lazy dog.
+---
